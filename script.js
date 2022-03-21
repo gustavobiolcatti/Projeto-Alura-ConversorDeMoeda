c5aaf8be8f9f510c3ff49095b57c04515e924d3c
Gustavo Biolcatti <43222626+gustavobiolcatti@users.noreply.github.com>
2022-03-21 17:45:26 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+function converterMoeda() {
+	const valorReal = parseFloat(document.getElementById("valor").value);
+
+	const valorDolar = 5.0;
+	const valorEuro = 5.5;
+	const valorBitcoin = 195482.0;
+
+	const convertidoDolar = (valorReal / valorDolar).toFixed(2);
+	const convertidoEuro = (valorReal / valorEuro).toFixed(2);
+	const convertidoBitcoin = (valorReal / valorBitcoin).toFixed(6);
+
+	document.getElementById(
+		"resultado__texto--dolar"
+	).innerHTML = `$ ${convertidoDolar}`;
+	document.getElementById(
+		"resultado__texto--euro"
+	).innerHTML = `€ ${convertidoEuro}`;
+	document.getElementById(
+		"resultado__texto--bitcoin"
+	).innerHTML = `BTC ${convertidoBitcoin}`;
+}
+
+function obterCotacao() {
+	const txtDolar = document.getElementById("cotacao__texto--dolar");
+	const txtEuro = document.getElementById("cotacao__texto--euro");
+	const txtBitcoin = document.getElementById("cotacao__texto--bitcoin");
+	
+	fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+		.then(resposta => resposta.json())
+		.then(json => imprimeResult(json));
+
+	function imprimeResult(json) {
+		for (let obj in json){
+			if (json[obj].code == "USD") {
+				txtDolar.innerHTML = `R$ ${parseFloat(json[obj].ask).toFixed(2)}`;
+			}
+			else if (json[obj].code == "EUR") {
+				txtEuro.innerHTML = `R$ ${parseFloat(json[obj].ask).toFixed(2)}`;
+			}
+			else if (json[obj].code == "BTC") {
+				txtBitcoin.innerHTML = `R$ ${parseFloat(json[obj].ask).toFixed(2)*1000}`;
+			}
+		}
+
+		console.log(json)
+	};
+
+}
